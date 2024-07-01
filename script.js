93b624d2c010203363040dfbcf1b36e0ab9894a4
dzhurar <romadzhura8822@gmail.com>
2024-07-01 17:16:30 +0300

    HW done

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const c = 25;
+const f = 1.8 * c + 32;
+console.log(f);
+
+const days = 31;
+const hours = days * 24;
+const minutes = hours * 60;
+console.log(minutes);
+
+let hp = 100;
+let stamina = 100;
+hp -= 43; 
+stamina -= 32;
+console.log(hp);
+console.log(stamina);
+
+let price = 220;
+let sum = price - price * 0.1;
+console.log(sum);
+
+let e = 2.71828182846;
+console.log(Math.floor(e));
+
+let damn = '13.37omgwth1917'
+console.log(parseFloat(damn));
+
+let age = '16 years 8 month'
+console.log(parseInt(age));
+
+let anyNumber = 81;
+console.log(Math.sqrt(anyNumber));
+
+let number = 21;
+let string = '2 more times'
+console.log(parseInt(string));
+console.log(number.toString());
